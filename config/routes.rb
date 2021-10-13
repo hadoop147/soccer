a27f006947b54e51fa35636eb2c42a57173de502
hadoop147 <hadoop147@icloud.com>
2021-10-13 12:21:52 +0000

    finished admin setup

--- config/routes.rb
+++ config/routes.rb
@@ -7,7 +7,8 @@ Rails.application.routes.draw do
 
   root to: redirect('/pitches')
   #resources :pitches
-  #If you don’t want your non-admin users to update your ideas you can easy fix this by updating your route file to only allow “index” and “show”. 
+  
+  #If you don’t want your non-admin users to update your pitches you can easy fix this by updating your route file to only allow “index” and “show”. 
   #Add only: [:show, :index] to config/route.rb:
   resources :pitches, only: [:show, :index] 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
